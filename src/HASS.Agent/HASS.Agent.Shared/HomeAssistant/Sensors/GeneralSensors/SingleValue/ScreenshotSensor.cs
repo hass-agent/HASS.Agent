5002f7a1620a9c62c984fb672ea945f73e43c4c3
Amadeo Alex <68441479+amadeo-alex@users.noreply.github.com>
2024-05-04 00:14:02 +0200

    added display scaling to screenshot size calculations

--- src/HASS.Agent/HASS.Agent.Shared/HomeAssistant/Sensors/GeneralSensors/SingleValue/ScreenshotSensor.cs
+++ src/HASS.Agent/HASS.Agent.Shared/HomeAssistant/Sensors/GeneralSensors/SingleValue/ScreenshotSensor.cs
@@ -13,11 +13,17 @@ using System.Xml.Linq;
 using System.Runtime.InteropServices;
 using System.Drawing.Imaging;
 using System.Text.RegularExpressions;
+using Windows.Graphics.Display;
 
 namespace HASS.Agent.Shared.HomeAssistant.Sensors.GeneralSensors.SingleValue;
 public class ScreenshotSensor : AbstractSingleValueSensor
 {
     private const string DefaultName = "screenshot";
+
+    private DEVMODE _devMode = new()
+    {
+        dmSize = (short)Marshal.SizeOf(typeof(DEVMODE))
+    };
 
     public int ScreenIndex;
 
@@ -78,10 +84,15 @@ public class ScreenshotSensor : AbstractSingleValueSensor
 
     private byte[] CapturePngFile(Screen screen)
     {
+        EnumDisplaySettings(screen.DeviceName, -1, ref _devMode);
+
+        var scalingFactor = Math.Round(decimal.Divide(_devMode.dmPelsWidth, screen.Bounds.Width), 2);
+
         var captureRectangle = screen.Bounds;
-        var captureBitmap = new Bitmap(captureRectangle.Width, captureRectangle.Height, PixelFormat.Format32bppArgb);
+        var captureSize = new Size(Convert.ToInt32(screen.Bounds.Width * scalingFactor), Convert.ToInt32(screen.Bounds.Height * scalingFactor));
+        using var captureBitmap = new Bitmap(captureSize.Width, captureSize.Height, PixelFormat.Format32bppArgb);
         var captureGraphics = Graphics.FromImage(captureBitmap);
-        captureGraphics.CopyFromScreen(captureRectangle.Left, captureRectangle.Top, 0, 0, captureRectangle.Size);
+        captureGraphics.CopyFromScreen(captureRectangle.Left, captureRectangle.Top, 0, 0, captureSize);
 
         using var ms = new MemoryStream();
         captureBitmap.Save(ms, ImageFormat.Png);
@@ -90,4 +101,46 @@ public class ScreenshotSensor : AbstractSingleValueSensor
     }
 
     public override string GetAttributes() => string.Empty;
+
+    [StructLayout(LayoutKind.Sequential)]
+    private struct DEVMODE
+    {
+        private const int CCHDEVICENAME = 0x20;
+        private const int CCHFORMNAME = 0x20;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 0x20)]
+        public string dmDeviceName;
+        public short dmSpecVersion;
+        public short dmDriverVersion;
+        public short dmSize;
+        public short dmDriverExtra;
+        public int dmFields;
+        public int dmPositionX;
+        public int dmPositionY;
+        public ScreenOrientation dmDisplayOrientation;
+        public int dmDisplayFixedOutput;
+        public short dmColor;
+        public short dmDuplex;
+        public short dmYResolution;
+        public short dmTTOption;
+        public short dmCollate;
+        [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 0x20)]
+        public string dmFormName;
+        public short dmLogPixels;
+        public int dmBitsPerPel;
+        public int dmPelsWidth;
+        public int dmPelsHeight;
+        public int dmDisplayFlags;
+        public int dmDisplayFrequency;
+        public int dmICMMethod;
+        public int dmICMIntent;
+        public int dmMediaType;
+        public int dmDitherType;
+        public int dmReserved1;
+        public int dmReserved2;
+        public int dmPanningWidth;
+        public int dmPanningHeight;
+    }
+
+    [DllImport("user32.dll")]
+    private static extern bool EnumDisplaySettings(string lpszDeviceName, int iModeNum, ref DEVMODE lpDevMode);
 }
