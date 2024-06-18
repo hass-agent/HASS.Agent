a8c4baa81f0a2eb7cb5af07514e21d9f60c0b0e6
Amadeo Alex <68441479+amadeo-alex@users.noreply.github.com>
2024-06-18 12:14:32 +0200

    poc

--- src/HASS.Agent/HASS.Agent.Shared/HomeAssistant/Commands/InternalCommands/MonitorSleepCommand.cs
+++ src/HASS.Agent/HASS.Agent.Shared/HomeAssistant/Commands/InternalCommands/MonitorSleepCommand.cs
@@ -2,9 +2,14 @@
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
+using System.Threading;
+using System.Windows.Forms;
+using System.Windows.Input;
 using HASS.Agent.Shared.Enums;
 using HASS.Agent.Shared.Functions;
 using Serilog;
+using Vanara.PInvoke;
+using static Vanara.PInvoke.PowrProf;
 
 namespace HASS.Agent.Shared.HomeAssistant.Commands.InternalCommands
 {
@@ -14,6 +19,8 @@ namespace HASS.Agent.Shared.HomeAssistant.Commands.InternalCommands
     [SuppressMessage("ReSharper", "InconsistentNaming")]
     public class MonitorSleepCommand : InternalCommand
     {
+        private static readonly HKEY s_key = new HKEY();
+
         private const string DefaultName = "monitorsleep";
 
         public MonitorSleepCommand(string entityName = DefaultName, string name = DefaultName, CommandEntityType entityType = CommandEntityType.Button, string id = default) : base(entityName ?? DefaultName, name ?? null, string.Empty, entityType, id)
@@ -25,7 +32,30 @@ namespace HASS.Agent.Shared.HomeAssistant.Commands.InternalCommands
         {
             State = "ON";
 
-            NativeMethods.PostMessage(NativeMethods.HWND_BROADCAST, NativeMethods.WM_SYSCOMMAND, (IntPtr)NativeMethods.SC_MONITORPOWER, (IntPtr)2);
+            //NativeMethods.PostMessage(NativeMethods.HWND_BROADCAST, NativeMethods.WM_SYSCOMMAND, (IntPtr)NativeMethods.SC_MONITORPOWER, (IntPtr)2);
+
+            PowerGetActiveScheme(out var activeScheme);
+
+            var schemes = PowerEnumerate<Guid>(null, null);
+            foreach (var scheme in schemes)
+            {
+                var name = PowerReadFriendlyName(scheme);
+                if (name.EndsWith(" - HASS.Agent Monitor Sleep"))
+                    PowerDeleteScheme(s_key, scheme);
+            }
+
+            PowerDuplicateScheme(s_key, activeScheme, out var duplicateScheme);
+            var duplicatedSchemeGuid = duplicateScheme.ToStructure<Guid>();
+            var newDuplicatedName = PowerReadFriendlyName(activeScheme) + " - HASS.Agent Monitor Sleep";
+            PowerWriteFriendlyName(duplicatedSchemeGuid, null, null, newDuplicatedName);
+
+            PowerWriteACValueIndex(s_key, duplicatedSchemeGuid, GUID_VIDEO_SUBGROUP, GUID_VIDEO_POWERDOWN_TIMEOUT, 1);
+            PowerSetActiveScheme(s_key, duplicatedSchemeGuid);
+
+            Thread.Sleep(1500);
+
+            PowerSetActiveScheme(s_key, activeScheme);
+            PowerDeleteScheme(s_key, duplicatedSchemeGuid);
 
             State = "OFF";
         }
