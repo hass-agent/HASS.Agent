f0e61b38039916f346c165ed76cc9dc9c5cd7888
Denis <denis@denis.ch>
2024-08-10 17:16:51 +0200

    Add Memory cache to geolocation call

--- src/HASS.Agent/HASS.Agent/HomeAssistant/Sensors/GeneralSensors/SingleValue/GeoLocationSensor.cs
+++ src/HASS.Agent/HASS.Agent/HomeAssistant/Sensors/GeneralSensors/SingleValue/GeoLocationSensor.cs
@@ -1,5 +1,9 @@
-﻿using HASS.Agent.Shared.Extensions;
+﻿using System.Runtime.Caching;
+using HASS.Agent.Shared.Extensions;
 using HASS.Agent.Shared.Models.HomeAssistant;
+using Windows.Devices.Geolocation;
+using HASS.Agent.Models.Internal;
+using Newtonsoft.Json;
 
 namespace HASS.Agent.HomeAssistant.Sensors.GeneralSensors.SingleValue
 {
@@ -9,7 +13,17 @@ namespace HASS.Agent.HomeAssistant.Sensors.GeneralSensors.SingleValue
     public class GeoLocationSensor : AbstractSingleValueSensor
     {
         private const string DefaultName = "geolocation";
-        public GeoLocationSensor(int? updateInterval = 10, string entityName = DefaultName, string name = DefaultName, string id = default) : base(entityName ?? DefaultName, name ?? null, updateInterval ?? 30, id) { }
+        private string state = "OFF";
+
+        public GeoLocationSensor(int? updateInterval = 10, string name = DefaultName, string friendlyName = DefaultName,
+            string id = default) : base(name ?? DefaultName, friendlyName ?? null, updateInterval ?? 30, id)
+        {
+            Domain = "device_tracker";
+            UseAttributes = true;
+
+        }
+
+        private string _attributes;
 
         public override DiscoveryConfigModel GetAutoDiscoveryConfig()
         {
@@ -17,32 +31,93 @@ namespace HASS.Agent.HomeAssistant.Sensors.GeneralSensors.SingleValue
 
             var deviceConfig = Variables.MqttManager.GetDeviceConfigModel();
             if (deviceConfig == null) return null;
-
+            //WIP
+            Domain = "device_tracker";
             return AutoDiscoveryConfigModel ?? SetAutoDiscoveryConfigModel(new SensorDiscoveryConfigModel()
             {
-                EntityName = EntityName,
+                EntityName = Name,
                 Name = Name,
                 Unique_id = Id,
                 Device = deviceConfig,
                 State_topic = $"{Variables.MqttManager.MqttDiscoveryPrefix()}/{Domain}/{deviceConfig.Name}/{ObjectId}/state",
                 Icon = "mdi:earth",
-                Availability_topic = $"{Variables.MqttManager.MqttDiscoveryPrefix()}/{Domain}/{deviceConfig.Name}/availability"
+                Json_attributes_topic = $"{Variables.MqttManager.MqttDiscoveryPrefix()}/{Domain}/{deviceConfig.Name}/{ObjectId}/attributes",
             });
         }
 
         public override string GetState()
         {
-            var locator = new Windows.Devices.Geolocation.Geolocator();
-            var location = locator.GetGeopositionAsync().GetAwaiter().GetResult();
-            var position = location.Coordinate.Point.Position;
+            var state = "OFF";
+            state = "ON";
 
-            var lat = position.Latitude.ConvertToStringDotDecimalSeperator();
-            var lon = position.Longitude.ConvertToStringDotDecimalSeperator();
-            var alt = position.Altitude.ConvertToStringDotDecimalSeperator();
-
-            return $"{lat},{lon},{alt}";
+            return state;
         }
 
-        public override string GetAttributes() => string.Empty;
+        public void SetAttributes(string value) => _attributes = string.IsNullOrWhiteSpace(value) ? "{}" : value;
+        //public override string GetAttributes() => _attributes;
+
+        
+        public override string? GetAttributes()
+        {
+            ObjectCache cache = MemoryCache.Default;
+            GeolocationInfo fileContents = cache["location_" + this.Id] as GeolocationInfo;
+            var state = "OFF";
+
+            if (fileContents != null)
+            {
+                string jsonPayload = JsonConvert.SerializeObject(fileContents, Formatting.Indented);
+                SetAttributes(jsonPayload);
+                state = "ON";
+                return jsonPayload;
+            }
+            else
+            {
+                Geolocator geolocator = null;
+                Geoposition position;
+                GeolocationInfo gli = new GeolocationInfo();
+
+                var accessStatus = Geolocator.RequestAccessAsync().GetAwaiter().GetResult();
+                switch (accessStatus)
+                {
+                    case GeolocationAccessStatus.Allowed:
+                        // notify user: Waiting for update
+
+                        //https://community.home-assistant.io/t/attributes-latitude-and-longitude-in-a-lovelace-map/318760
+                        geolocator = new Geolocator();
+                        position = geolocator.GetGeopositionAsync().GetAwaiter().GetResult();
+                        var lat = position.Coordinate.Latitude.ConvertToStringDotDecimalSeperator();
+                        var lon = position.Coordinate.Longitude.ConvertToStringDotDecimalSeperator();
+                        var alt = position.Coordinate.Altitude?.ConvertToStringDotDecimalSeperator();
+                        var accuracy = position.Coordinate.Accuracy.ConvertToStringDotDecimalSeperator();
+                        var sourceType = position.Coordinate.PositionSource;
+
+                        gli = new GeolocationInfo(lon, lat, sourceType);
+                        gli.gps_accuracy = accuracy;
+                        gli.not_permitted = "false";
+
+                        cache.Set("location_" + this.Id, gli, DateTimeOffset.Now.AddSeconds(120));
+
+                        state = "ON";
+
+                        break;
+
+                    case GeolocationAccessStatus.Denied:
+                        // notify user: Access to location is denied
+                        gli.not_permitted = "true";
+
+                        break;
+
+                    case GeolocationAccessStatus.Unspecified:
+                        // notify user: Unspecified error
+                        gli.not_permitted = "true";
+
+                        break;
+                }
+
+                string json_payload = JsonConvert.SerializeObject(gli, Formatting.Indented);
+                SetAttributes(json_payload);
+                return json_payload;
+            }
+        }
     }
 }
---
+++ src/HASS.Agent/HASS.Agent/Models/Internal/GeolocationInfo.cs
@@ -0,0 +1,37 @@
+﻿using Windows.Devices.Geolocation;
+
+namespace HASS.Agent.Models.Internal
+{
+    public class GeolocationInfo
+    {
+        public string latitude { get; set; }
+        public string longitude { get; set; }
+        public string source_type { get; set; }
+        public string gps_accuracy { get; set; }
+        public string not_permitted { get; set; }
+
+        public GeolocationInfo()
+        {
+
+        }
+
+        public GeolocationInfo(string lon, string lat, PositionSource source)
+        {
+            longitude = lon;
+            latitude = lat;
+            source_type = "";
+            gps_accuracy = "1.2";
+
+            switch (source )
+            {
+                case PositionSource.Satellite:
+                    source_type = "gps";
+                    break;
+                case PositionSource.WiFi:
+                    source_type = "router";
+                    break;
+            }
+        }
+    }
+}
+    
