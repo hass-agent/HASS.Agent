58985b779bca95c83d536a9688b4b097ed07c891
Amadeo Alex <68441479+amadeo-alex@users.noreply.github.com>
2025-06-13 19:53:45 +0200

    format

--- src/HASS.Agent/HASS.Agent/Controls/Onboarding/Onboarding-6-HotKey.cs
+++ src/HASS.Agent/HASS.Agent/Controls/Onboarding/Onboarding-6-HotKey.cs
@@ -17,30 +17,41 @@ namespace HASS.Agent.Controls.Onboarding
             // config quick actions hotkey selector
             _hotkeySelector.Enable(TbQuickActionsHotkey);
 
-            // if nothing set, load default
-            if (string.IsNullOrEmpty(Variables.AppSettings.QuickActionsHotKey)) LoadDefault();
-            // if set to empty, show empty
-            else if (Variables.AppSettings.QuickActionsHotKey == _hotkeySelector.EmptyHotkeyText) TbQuickActionsHotkey.Text = _hotkeySelector.EmptyHotkeyText;
-            // show set value
-            else LoadSetValue();
+
+            if (string.IsNullOrEmpty(Variables.AppSettings.QuickActionsHotKey))
+            {
+                // if nothing set, load default
+                LoadDefault();
+            }
+            else if (Variables.AppSettings.QuickActionsHotKey == _hotkeySelector.EmptyHotkeyText)
+            {
+                // if set to empty, show empty
+                TbQuickActionsHotkey.Text = _hotkeySelector.EmptyHotkeyText;
+            }
+            else
+            {
+                // show set value
+                LoadSetValue();
+            }
         }
 
         private void LoadDefault()
         {
-            if (!HelperFunctions.InputLanguageCheckDiffers(out var knownToCollide, out var warning))
-            {
-                TbQuickActionsHotkey.Text = "Shift, Alt + Q";
-                LblLanguageWarning.Visible = false;
-                return;
-            }
+            /*            if (!HelperFunctions.InputLanguageCheckDiffers(out var knownToCollide, out var warning))
+                        {
+                            TbQuickActionsHotkey.Text = "Shift, Alt + Q";
+                            LblLanguageWarning.Visible = false;
+                            return;
+                        }
 
-            if (knownToCollide)
-            {
-                // the system's input language collides with our hotkey, let the user know and set empty key
-                LblLanguageWarning.Text = warning;
-                TbQuickActionsHotkey.Text = _hotkeySelector.EmptyHotkeyText;
-                return;
-            }
+                        if (knownToCollide)
+                        {
+                            // the system's input language collides with our hotkey, let the user know and set empty key
+                            LblLanguageWarning.Text = warning;
+                            TbQuickActionsHotkey.Text = _hotkeySelector.EmptyHotkeyText;
+                            return;
+                        }*/
+            //Amadeo(Note): above was commented out when we changed default hotkey to ctrl+shift+q, leaving this here because reasons
 
             // the system's input language is unknown, we're presetting the default but warn the user
             // deprecated, we're not doing this anymore
@@ -56,12 +67,15 @@ namespace HASS.Agent.Controls.Onboarding
         {
             TbQuickActionsHotkey.Text = Variables.AppSettings.QuickActionsHotKey;
 
-            if (!HelperFunctions.InputLanguageCheckDiffers(out var knownToCollide, out var warning)) return;
+            if (!HelperFunctions.InputLanguageCheckDiffers(out var knownToCollide, out var warning))
+                return;
 
             // the system's input language is unknown or collides with our hotkey, let the user know if it's set to default
-            if (Variables.AppSettings.QuickActionsHotKey != "Control, Alt + Q") return;
+            if (Variables.AppSettings.QuickActionsHotKey != "Control, Alt + Q")
+                return;
 
-            if (knownToCollide) LblLanguageWarning.Text = warning;
+            if (knownToCollide)
+                LblLanguageWarning.Text = warning;
         }
 
         internal bool Store()
