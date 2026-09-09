a9f2519e61edcfeb8d0582355fafb65c3fa0cec4
amadeo-alex <68441479+amadeo-alex@users.noreply.github.com>
2026-09-09 14:09:57 +0200

    Fix: Virtual Desktop library crashing HASS.Agent on unsupported systems (#485)

--- src/HASS.Agent/HASS.Agent/Managers/VirtualDesktopManager.cs
+++ src/HASS.Agent/HASS.Agent/Managers/VirtualDesktopManager.cs
@@ -13,12 +13,20 @@ namespace HASS.Agent.Managers;
 //Note(Amadeo): due to poor m$ decisions on availability of the Virtual Desktop management APIs, there is no good way to manage Virtual Desktops with publicly available ones.
 //We need to rely on on Community made ones which can stop working with new Windows version due to changes in the COM object name changes.
 //This means it's required to limit the functionality on systems not to expose users to runtime application errors.
+//Update(09.2026): what is more, looks like the library has a taskbar restart hook that causes HASS.Agent to crash when taskbar is recreated...
 internal static class VirtualDesktopManager
 {
     public static bool Initialized { get; private set; } = false;
 
-    internal static bool Initialize()
+    internal static void Initialize()
     {
+        if (Variables.AppSettings.DisableVirtualDesktopInitialization)
+        {
+            Initialized = false;
+            Log.Information("[VIRTDESKT] Virtual Desktop Manager initialization disabled");
+            return;
+        }
+        
         try
         {
             VirtualDesktop.Configure();
@@ -28,10 +36,8 @@ internal static class VirtualDesktopManager
         catch
         {
             Initialized = false;
-            Log.Error("[VIRTDESKT] Error initializing Virtual Desktop Manager, your Windows version may be unsupported"); //TODO(Amadeo): add link to documentation explaining the issue.
+            Log.Error("[VIRTDESKT] Error initializing Virtual Desktop Manager, your Windows version may be unsupported, please disable initialization in HASS.Agent startup settings!"); //TODO(Amadeo): add link to documentation explaining the issue.
         }
-
-        return Initialized;
     }
 
     internal static void ActivateDesktop(string virtualDesktopId)
@@ -39,10 +45,10 @@ internal static class VirtualDesktopManager
         if (!Initialized)
         {
             Log.Warning("[VIRTDESKT] Cannot activate virtual desktop, manager not initialized");
+            return;
         }
 
-        Guid targetDesktopGuid;
-        var parsed = Guid.TryParse(virtualDesktopId, out targetDesktopGuid);
+        var parsed = Guid.TryParse(virtualDesktopId, out var targetDesktopGuid);
         if (!parsed)
         {
             Log.Warning("[VIRTDESKT] Unable to parse virtual desktop id: {virtualDesktopId}", virtualDesktopId);
@@ -57,6 +63,7 @@ internal static class VirtualDesktopManager
         if (!Initialized)
         {
             Log.Warning("[VIRTDESKT] Cannot activate virtual desktop, manager not initialized");
+            return;
         }
 
         try
@@ -111,6 +118,12 @@ internal static class VirtualDesktopManager
     {
         var desktops = new Dictionary<string, string>();
 
+        if (!Initialized)
+        {
+            Log.Warning("[VIRTDESKT] Cannot get all virtual desktops, manager not initialized");
+            return desktops;
+        }
+        
         try
         {
             foreach (var desktop in VirtualDesktop.GetDesktops())
--- src/HASS.Agent/HASS.Agent/Models/Config/AppSettings.cs
+++ src/HASS.Agent/HASS.Agent/Models/Config/AppSettings.cs
@@ -86,5 +86,7 @@ namespace HASS.Agent.Models.Config
 
         public bool NfcScanningEnabled { get; set; } = false;
         public string NfcSelectedScanner { get; set; } = string.Empty;
+
+        public bool DisableVirtualDesktopInitialization { get; set; } = false;
     }
 }
